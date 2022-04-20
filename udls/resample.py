963ecdbd48ff564d957346923424e4137cbfa21c
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2022-04-20 11:44:56 -0400

    Add: Comment for ffmpeg preprocessing.

--- udls/resample.py
+++ udls/resample.py
@@ -41,6 +41,10 @@ def main():
             out_name = f"audio_{i:05d}_%05d.wav"
             cmd = f"ffmpeg -loglevel panic -hide_banner "
             cmd += f"-i \"{elm}\" -f segment -segment_time {args.len} "
+            # Remove silence from the beginning, middle or end of the audio. 
+            # https://ffmpeg.org/ffmpeg-filters.html#silenceremove
+            # stop_periods removes silence from the middle of a file with negative value. 
+            # stop_threshold trims silence from the end of audio.
             cmd += "-af \"dynaudnorm, silenceremove=stop_periods=-1:stop_duration=1:stop_threshold=-60dB\" "
             cmd += f"-ar {args.sr} -ac 1 {path.join(out_dir, out_name)}"
 
